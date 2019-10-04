f49c916faedbe497be74a290547b03635978f17a
tommarksmi <thomasmarksmithiii@gmail.com>
2019-10-04 09:09:45 -0400

    Pull data from post operation, change todo resource url

--- api.py
+++ api.py
@@ -6,7 +6,7 @@ from todo import Todo
 app = Flask(__name__)
 api = Api(app)
 
-api.add_resource(Todo, '/')
+api.add_resource(Todo, '/todo/<todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
--- todo.py
+++ todo.py
@@ -1,6 +1,14 @@
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
+from flask import Flask, request
+
+parser = reqparse.RequestParser()
 
 
 class Todo(Resource):
-    def get(self):
-        return {'test': 'testing stuff'}
+    def get(self, todo_id):
+        return {todo_id: 'testing stuff'}
+
+    def post(self, todo_id):
+        request_data = request.form['data']
+        # args = parser.parse_args()
+        return request_data
